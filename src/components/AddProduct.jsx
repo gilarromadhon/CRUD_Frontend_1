b0b04205e1c1fe5f44c6ac749d377a9da4d742b8
Gilar Romadhon <gilarromadhon@gmail.com>
2022-11-16 21:03:34 +0700

    first commit

---
+++ src/components/AddProduct.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const AddProduct = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("");
+  const navigate = useNavigate();
+
+  const saveProduct = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:5001/users", {
+      name: name,
+      email: email,
+      gender: gender,
+    });
+    navigate("/");
+  };
+
+  return (
+    <div className="max-w-lg mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
+      <form onSubmit={saveProduct} className="my-10">
+        <div className="flex flex-col">
+          <div className="mb-5">
+            <label className="font-bold text-slate-700">Name</label>
+            <input
+              type="text"
+              className="w-full py-3 mt-1 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-5">
+            <label className="font-bold text-slate-700">Email</label>
+            <input
+              type="text"
+              className="w-full py-3 mt-1 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-5">
+            <label className="font-bold text-slate-700">Gender</label>
+            <div className="control">
+              <div className="w-full py-3 mt-1 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow">
+                <select
+                  className="w-full focus:outline-none"
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                >
+                  <option value="">Gender</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="w-full py-3 font-bold text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddProduct;
